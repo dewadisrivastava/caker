919d8a5c07d843c9ec492cd5347a8b3a31079b26
dewadisrivastava <82640409+dewadisrivastava@users.noreply.github.com>
2021-05-16 11:09:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+
+function setup(){
+    createCanvas(400,400)
+
+}
+
+function draw(){
+    background(0)
+    text("1)have some milk and flour in a bowl",100,200)
+    text("2)mix it with your flavour according to you and add sugar",100,215);
+    text("3)keep it in the oven and bake it for atleast 25 mins",100,230);
+    text("ingridients : red velvet cake flavour, flour,sugar,baking soda,milk",20,270)
+
+    strokeWeight(3)
+    stroke("red")
+}
